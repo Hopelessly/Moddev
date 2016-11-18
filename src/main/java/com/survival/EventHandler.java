101e877ec4679ebce6e9844c3b835022a0108845
Hopelessly <Lastinglyhopeless@gmail.com>
2016-11-18 14:36:36 -0800

    small fix

--- src/main/java/com/survival/EventHandler.java
+++ src/main/java/com/survival/EventHandler.java
@@ -15,51 +15,55 @@ import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
 
 public class EventHandler {
 	@SubscribeEvent
-    public void onPlayerLogsIn(PlayerLoggedInEvent event)
-    {
-        EntityPlayer player = event.player;
-        IThirst thirst = player.getCapability(ThirstProvider.Thirst_CAP, null);
+	public void onPlayerLogsIn(PlayerLoggedInEvent event) {
+		EntityPlayer player = event.player;
+		IThirst thirst = player.getCapability(ThirstProvider.Thirst_CAP, null);
 
-        String message = String.format("Hello there, you have " + (int) thirst.getThirst()) +" thirst left.";
-        player.addChatMessage(new TextComponentString(message));
-    }
+		String message = String.format("Hello there, you have " + (int) thirst.getThirst()) + " thirst left.";
+		player.addChatMessage(new TextComponentString(message));
+	}
 
-    @SubscribeEvent
-    public void onPlayerSleep(PlayerSleepInBedEvent event)
-    {
-        EntityPlayer player = event.getEntityPlayer();
+	@SubscribeEvent
+	public void onPlayerSleep(PlayerSleepInBedEvent event) {
+		EntityPlayer player = event.getEntityPlayer();
 
-        if (player.worldObj.isRemote) return;
+		if (player.worldObj.isRemote)
+			return;
+		if (player.isPlayerFullyAsleep()) {
+			IThirst thirst = player.getCapability(ThirstProvider.Thirst_CAP, null);
 
-        IThirst thirst = player.getCapability(ThirstProvider.Thirst_CAP, null);
+			thirst.fill(50);
 
-        thirst.fill(50);
+			String message = String.format(
+					"You refreshed yourself in the bed. You received 50 Thirst, you have " + (int) thirst.getThirst())
+					+ " thirst left.";
+			player.addChatMessage(new TextComponentString(message));
+		}
+	}
 
-        String message = String.format("You refreshed yourself in the bed. You received 50 Thirst, you have " + (int) thirst.getThirst()) +" thirst left.";
-        player.addChatMessage(new TextComponentString(message));
-    }
-
-//    @SubscribeEvent
-//    public void onPlayerFalls(LivingFallEvent event)
-//    {
-//        Entity entity = event.getEntity();
-//
-//        if (entity.worldObj.isRemote || !(entity instanceof EntityPlayerMP) || event.getDistance() < 3) return;
-//
-//        EntityPlayer player = (EntityPlayer) entity;
-//        IThirst thirst = player.getCapability(ThirstProvider.Thirst_CAP, null);
-//
-//        float points = thirst.getThirst();
-//        float cost = event.getDistance() * 2;
-//
-//        if (points > cost)
-//        {
-//            thirst.consume(cost);
-//
-//            String message = String.format("You absorbed fall damage. It costed , you have  left.", (int) cost, (int) thirst.Thirst());
-//            player.addChatMessage(new TextComponentString(message));
-//
-//            event.setCanceled(true);
-//        }
-//    }
+	// @SubscribeEvent
+	// public void onPlayerFalls(LivingFallEvent event)
+	// {
+	// Entity entity = event.getEntity();
+	//
+	// if (entity.worldObj.isRemote || !(entity instanceof EntityPlayerMP) ||
+	// event.getDistance() < 3) return;
+	//
+	// EntityPlayer player = (EntityPlayer) entity;
+	// IThirst thirst = player.getCapability(ThirstProvider.Thirst_CAP, null);
+	//
+	// float points = thirst.getThirst();
+	// float cost = event.getDistance() * 2;
+	//
+	// if (points > cost)
+	// {
+	// thirst.consume(cost);
+	//
+	// String message = String.format("You absorbed fall damage. It costed , you
+	// have left.", (int) cost, (int) thirst.Thirst());
+	// player.addChatMessage(new TextComponentString(message));
+	//
+	// event.setCanceled(true);
+	// }
+	// }
 }
--- src/main/java/com/survival/Survival.java
+++ src/main/java/com/survival/Survival.java
@@ -10,10 +10,10 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 @Mod(modid = Survival.MODID, version = Survival.VERSION)
 public class Survival
 {
-    public static final String MODID = "capabilities-test";
-    public static final String VERSION = "0.1";
+    public static final String MODID = "thirst-test";
+    public static final String VERSION = "1.0";
 
-    @SidedProxy(clientSide = "mchorse.capabilities.CommonProxy", serverSide = "mchorse.capabilities.CommonProxy")
+    @SidedProxy(clientSide = "com.survival.proxy.CommonProxy", serverSide = "com.survival.proxy.CommonProxy")
     public static CommonProxy proxy;
 
     @EventHandler
