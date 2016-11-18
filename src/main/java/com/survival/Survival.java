881c3751b4fd6812418523cc4140fb1c66444cc2
Hopelessly <Lastinglyhopeless@gmail.com>
2016-11-18 15:16:52 -0800

    upload of only src

--- src/main/java/com/survival/Survival.java
+++ src/main/java/com/survival/Survival.java
@@ -7,10 +7,11 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = Survival.MODID, version = Survival.VERSION)
+@Mod(modid = Survival.MODID, version = Survival.VERSION, name = Survival.NAME)
 public class Survival
 {
     public static final String MODID = "thirst-test";
+    public static final String NAME = "Survival";
     public static final String VERSION = "1.0";
 
     @SidedProxy(clientSide = "com.survival.proxy.CommonProxy", serverSide = "com.survival.proxy.CommonProxy")
