f7a4e7c073ab99f53d0d9d2cdd784e80640835b6
Hopelessly <Lastinglyhopeless@gmail.com>
2016-11-20 12:49:48 -0800

    updated events

--- src/main/java/com/survival/Survival.java
+++ src/main/java/com/survival/Survival.java
@@ -13,9 +13,9 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = Survival.MODID, version = Survival.VERSION, name = Survival.NAME)
 public class Survival
 {
-    public static final String MODID = "thirst-test";
+    public static final String MODID = "alpha";
     public static final String NAME = "Survival";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     public static final float SLEEP_MAX = 1000F;
     public static final float THIRST_MAX = 1000F;
 
@@ -29,7 +29,7 @@ public class Survival
         proxy.init();
     }
     
-    @Mod.EventHandler
+    @EventHandler
     public void preInit(FMLPreInitializationEvent event) 
     {
     	ModItems.init();
